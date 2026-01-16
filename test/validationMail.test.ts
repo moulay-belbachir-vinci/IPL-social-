907213315071b17ec4d38ecb7fc8b04a3a4440cd
unknown <moulay.belbachir@student.vinci.be>
2026-01-16 16:30:26 +0100

    add : jest.config.js and first test verify if @ works

--- test/validationMail.test.ts
+++ test/validationMail.test.ts
@@ -5,14 +5,14 @@ describe('Validation Email', ()=>{
 
     describe('validate an email', ()=> {
 
-        it("should return true if the email contain @ "), ()=> {
+        it("should return true if the email contain @ ", ()=> {
 
             const validationMail = new ValidationMail;
 
             const actual = validationMail.verifyString("@");
 
             expect(actual).toBe(true);
-        }
+        })
         
     })
 })
